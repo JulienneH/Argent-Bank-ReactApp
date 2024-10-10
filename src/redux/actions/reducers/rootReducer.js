3732e3e86ac4282ac2cc2e59aa9acec120a6d301
Julienne H <juliennehattabi@gmail.com>
2024-10-10 10:48:04 +0200

    refactor: Remove userProfile reducer as it was redundant

--- src/redux/actions/reducers/rootReducer.js
+++ src/redux/actions/reducers/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 
 import authentificationReducer from "../reducers/authentificationReducer";
-import userProfileReducer from "../reducers/userProfileReducer";
 
 const rootReducer = combineReducers({
   auth: authentificationReducer,
-  userProfile: userProfileReducer,
 });
 
 export default rootReducer;
