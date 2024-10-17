beeea6573bb58fb1043e869c5bdce0b38229c569
Julienne H <juliennehattabi@gmail.com>
2024-10-17 09:33:40 +0200

    feat : added confirmation message when user changes username

--- src/components/FormUserInfo.jsx
+++ src/components/FormUserInfo.jsx
@@ -4,6 +4,7 @@ import { updateUsername } from "../redux/actions/usernameAction";
 
 const FormUserInfo = () => {
   const [username, setUsername] = useState("");
+  const [confirmationMessage, setConfirmationMessage] = useState("");
   const dispatch = useDispatch();
   const token = useSelector((state) => state.token);
   const currentUsername = useSelector((state) => state.username);
@@ -11,8 +12,12 @@ const FormUserInfo = () => {
     event.preventDefault();
 
     if (token && username) {
-      await dispatch(updateUsername({ username, token }));
-      console.log("username modifié");
+      try {
+        await dispatch(updateUsername({ username, token })).unwrap();
+        setConfirmationMessage("The username has been successfully changed.");
+      } catch (error) {
+        setConfirmationMessage("Failed to update username.");
+      }
     }
   };
 
@@ -20,35 +25,39 @@ const FormUserInfo = () => {
     event.preventDefault();
     // Réinitialiser le champ de username à la valeur actuelle
     setUsername(currentUsername);
+    setConfirmationMessage("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="input-wrapper">
-        <label>User name</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)} // Met à jour l'état local
-        />
-      </div>
-      <div className="input-wrapper">
-        <label>First Name</label>
-        <input type="text" />
-      </div>
-      <div className="input-wrapper">
-        <label>Last Name</label>
-        <input type="text" />
-      </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="input-wrapper">
+          <label>User name</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)} // Met à jour l'état local
+          />
+        </div>
+        <div className="input-wrapper">
+          <label>First Name</label>
+          <input type="text" />
+        </div>
+        <div className="input-wrapper">
+          <label>Last Name</label>
+          <input type="text" />
+        </div>
 
-      <button type="submit" className="sign-in-button">
-        Save
-      </button>
-      <button onClick={handleCancel} className="sign-in-button">
-        Cancel
-      </button>
-    </form>
+        <button type="submit" className="sign-in-button">
+          Save
+        </button>
+        <button onClick={handleCancel} className="sign-in-button">
+          Cancel
+        </button>
+      </form>
+      {confirmationMessage && <p>{confirmationMessage}</p>}
+    </>
   );
 };
 
