1022ffc58eb84a4bc316f0ab017ee08b51bce524
Julienne H <juliennehattabi@gmail.com>
2024-10-18 10:27:37 +0200

    feat : added props in the CardsAccounts component to anticipate the retrieval and display of account information

--- src/components/CardsAccounts.jsx
+++ src/components/CardsAccounts.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const CardsAccounts = () => {
+const CardsAccounts = ({ balance, accountName, transactionCount }) => {
   return (
     <section className="cards">
       <div className="cards_content">
-        <p className="cards_title">Argent Bank Checking (X3448)</p>
-        <p className="cards_amount">$49,098.43</p>
+        <p className="cards_title">
+          {accountName}
+          Argent Bank Checking (X3448{transactionCount})
+        </p>
+        <p className="cards_amount">$49,098.43{balance}</p>
         <p className="cards_balance">Available balance</p>
       </div>
       <i className="fa-solid fa-chevron-right cards_icon"></i>
